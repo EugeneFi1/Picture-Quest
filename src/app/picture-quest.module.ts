86698596763a47c27bbfafb34990e466fc08e9a0
EugeneFi1 <51033432+EugeneFi1@users.noreply.github.com>
2020-12-28 23:21:28 +0200

    routing

--- src/app/picture-quest.module.ts
+++ src/app/picture-quest.module.ts
@@ -1,19 +1,32 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+
 import { FormsModule } from "@angular/forms";
 import { QuestEditComponent } from "./edit-modules/quest-edit.component";
 import { QuestViewComponent } from "./view-modules/quest-view.component";
+import { InsertPictureComponent } from "./edit-modules/insert-picture/insert-picture.component";
+
+
+const appRoutes: Routes = [
+  {path: "", component: QuestViewComponent},
+  {path: "edit", component: QuestEditComponent, children: [
+      {path: "insert-picture", component: InsertPictureComponent}
+    ]},
+];
 
 @NgModule({
   declarations: [
        QuestEditComponent,
-       QuestViewComponent
+       QuestViewComponent,
+       InsertPictureComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
-    FormsModule 
+    FormsModule
   ],
   providers: [],
-  bootstrap: [QuestEditComponent, QuestViewComponent]
+  bootstrap: [QuestViewComponent]
 })
 export class PictureQuestModule { }
